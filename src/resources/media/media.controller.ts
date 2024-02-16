b5c86f4262593ead67db2b30d0de0c44902a9ab9
Daltonic <darlingtongospel@gmail.com>
2024-02-16 16:36:46 +0100

    Uploads and downloads from renterd

--- src/resources/media/media.controller.ts
+++ src/resources/media/media.controller.ts
@@ -1,23 +1,27 @@
-import Controller from "@/utils/interfaces/controller.interface";
-import { NextFunction, Request, Response, Router } from "express";
-import HttpException from "@/utils/exceptions/HttpException";
-import { StatusCodes } from "http-status-codes";
-import { loggedIn } from "@/middlewares/index";
-import SiaService from "./sia.service";
-import { FileUpload } from "./media.interface";
+import Controller from '@/utils/interfaces/controller.interface'
+import { NextFunction, Request, Response, Router } from 'express'
+import HttpException from '@/utils/exceptions/HttpException'
+import { StatusCodes } from 'http-status-codes'
+import { loggedIn } from '@/middlewares/index'
+import SiaService from './sia.service'
+import { FileUpload } from './media.interface'
 
 class MediaController implements Controller {
-  public path = "/media";
-  public router = Router();
-  private siaService = new SiaService();
+  public path = '/media'
+  public router = Router()
+  private siaService = new SiaService()
 
   constructor() {
-    this.initialiseRoutes();
+    this.initialiseRoutes()
   }
 
   private initialiseRoutes() {
-    this.router.get(`${this.path}/sia/download`, [], this.sendFile);
-    this.router.post(`${this.path}/sia/upload`, [loggedIn], this.receiveFile);
+    this.router.get(
+      `${this.path}/sia/download/:folder/:fileId`,
+      [],
+      this.sendFile
+    )
+    this.router.post(`${this.path}/sia/upload`, [loggedIn], this.receiveFile)
   }
 
   private receiveFile = async (
@@ -27,44 +31,37 @@ class MediaController implements Controller {
   ): Promise<Response | void> => {
     try {
       if (!req.files) {
-        throw new HttpException(StatusCodes.BAD_REQUEST, "No file uploaded");
+        throw new HttpException(StatusCodes.NO_CONTENT, 'No file uploaded')
       }
-      const fileUpload: FileUpload = req.files.file as FileUpload;
-      const result = await this.siaService.uploadFile(fileUpload);
+      const fileUpload: FileUpload = req.files.file as FileUpload
+      const result = await this.siaService.uploadFile(fileUpload)
 
-      return res.status(StatusCodes.CREATED).json(result);
+      return res.status(StatusCodes.CREATED).json(result)
     } catch (e: any) {
-      next(new HttpException(StatusCodes.BAD_REQUEST, e.message));
+      next(new HttpException(StatusCodes.BAD_REQUEST, e.message))
     }
-  };
+  }
 
   private sendFile = async (
     req: Request,
     res: Response,
     next: NextFunction
   ): Promise<Response | void> => {
-    const { fileType, fileId } = req.query;
+    const { folder, fileId } = req.params
 
     try {
-      if (!fileType) {
-        throw new HttpException(
-          StatusCodes.BAD_REQUEST,
-          "File type is required"
-        );
+      if (!folder || !fileId) {
+        return res
+          .status(StatusCodes.BAD_REQUEST)
+          .json({ message: 'Folder or File ID not found' })
+      } else {
+        const result = await this.siaService.downloadFile(folder, fileId)
+        return result.pipe(res).status(StatusCodes.OK)
       }
-      if (!fileId) {
-        throw new HttpException(StatusCodes.BAD_REQUEST, "File Id is required");
-      }
-
-      const result = await this.siaService.downloadFile(
-        String(fileType),
-        String(fileId)
-      );
-      return res.status(StatusCodes.OK).json(result);
-    } catch (e: any) {
-      next(new HttpException(StatusCodes.BAD_REQUEST, e.message));
+    } catch (error: any) {
+      next(new HttpException(StatusCodes.BAD_REQUEST, error.message))
     }
-  };
+  }
 }
 
-export default MediaController;
+export default MediaController
