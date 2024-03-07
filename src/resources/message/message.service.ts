ab5cc35cccce2e261c05646824be87e1fa4a3a74
Gospel Darlington <darlingtongospel@gmail.com>
2024-03-07 12:56:27 +0100

    Adds a proper markdown documentation

--- src/resources/message/message.service.ts
+++ src/resources/message/message.service.ts
@@ -1,34 +1,34 @@
-import sendEmail, { generateEmail } from "@/utils/mailer";
-import { SendMessageInterface } from "./message.interface";
-import { contactMessage } from "@/utils/templates/mails";
-import log from "@/utils/logger";
+import sendEmail, { generateEmail } from '@/utils/mailer'
+import { SendMessageInterface } from './message.interface'
+import { contactMessage } from '@/utils/templates/mails'
+import log from '@/utils/logger'
 
 class MessageService {
   public async sendMessage(
     messageInput: SendMessageInterface
   ): Promise<void | Error> {
-    const { email, message, name } = messageInput;
+    const { email, message, name } = messageInput
 
     try {
       const contactMail = await generateEmail(
         { name, email, message },
         contactMessage
-      );
+      )
 
       const mailSendSuccess = await sendEmail(
-        "emma.osademe@gmail.com",
+        process.env.PLATFORM_EMAIL!,
         contactMail,
-        "Contact Message"
-      );
+        'Contact Message'
+      )
 
       if (!mailSendSuccess) {
-        throw new Error("Error sending message");
+        throw new Error('Error sending message')
       }
     } catch (e: any) {
-      log.error(e.message);
-      throw new Error(e.message || "Error sending message");
+      log.error(e.message)
+      throw new Error(e.message || 'Error sending message')
     }
   }
 }
 
-export default MessageService;
+export default MessageService
